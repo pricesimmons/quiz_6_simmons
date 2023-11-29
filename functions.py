c324ab7f7179010cae4b194a5f8b11018af95b67
pricesimmons <150076115+pricesimmons@users.noreply.github.com>
2023-11-29 14:34:44 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,14 +1,18 @@
 def add_numbers(num1, num2):
+    # Adds two numbers and returns the result
     return num1 + num2
 
 def subtract_numbers(num1, num2):
+    #Subtracts num2 from num1 and returns the result
     return num1 - num2
 
 def divide_numbers(num1, num2):
+    # Divides num1 by num2 and returns the result
     if num2 != 0:
         return num1 / num2
     else:
         return "Cannot divide by zero"
 
 def multiply_numbers(num1, num2):
+    # Multiplies two numbers and returns the result
     return num1 * num2
